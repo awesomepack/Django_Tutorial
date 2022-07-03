bb3d12520ba2d09998290ad6ad1cead21de77642
Merari Cisneros <meraridanielcisneros@gmail.com>
2022-07-02 19:23:14 -0700

    worked with a sqlLite database to render data into a template

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class LogMessage(models.Model):
+    
+    message = models.CharField(max_length= 300)
+    log_date = models.DateTimeField("Date Logged")
+
+
+    def __str__(self):
+        """ Returns a string representation fo a message"""
+        date = timezone.localtime(self.log_date)
+        return f"'{self.message}' logged on {date.strftime('%A, %d %B, %Y at %X')}"
